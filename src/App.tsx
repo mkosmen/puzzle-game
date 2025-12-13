e773e832c48e196c42004d24aaf7eedd25bace0e
Mehmet KOSMEN <mkosmen@gmail.com>
2025-12-14 01:09:08 +0300

    shuffle action is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import pImg from "./assets/images/image-1.jpg";
 import { getNewSize, get1Part, getRow } from "./util";
+import shuffle from "lodash.shuffle";
 
 interface PartSize {
   width: number;
   height: number;
-  top: number;
-  left: number;
+  // top: number;
+  // left: number;
+  bgPosX: number;
+  bgPosy: number;
 }
 
-function PartItem(partSize: PartSize) {
+function PartItem(partSize: PartSize & { top: number; left: number }) {
   return (
     <div
       className="absolute top-0 left-0 bg-no-repeat"
@@ -19,8 +22,8 @@ function PartItem(partSize: PartSize) {
         top: `${partSize.top}px`,
         left: `${partSize.left}px`,
         backgroundImage: `url(${pImg})`,
-        backgroundPositionX: -1 * partSize.left,
-        backgroundPositionY: -1 * partSize.top,
+        backgroundPositionX: partSize.bgPosX,
+        backgroundPositionY: partSize.bgPosy,
       }}
     >
       <span className="absolute w-full h-full bg-transparent hover:bg-amber-900 transition-all opacity-25"></span>
@@ -28,10 +31,26 @@ function PartItem(partSize: PartSize) {
   );
 }
 
+function getRowCol(m: number) {
+  const row = getRow(m) - 1;
+  const col = m - row * 8;
+
+  return [row, col];
+}
+
+function getTopLeft(m: number, pw: number, ph: number) {
+  const [row, col] = getRowCol(m);
+  const top = 1 * (row * ph);
+  const left = 1 * (col * pw);
+
+  return [top, left];
+}
+
 function App() {
   const [, setDimension] = useState({ width: 0, height: 0 });
   const [divSize, setDivSize] = useState({ width: 0, height: 0 });
   const [parts, setParts] = useState<PartSize[]>([]);
+  const [partSize, setPartSize] = useState({ width: 0, height: 0 });
 
   useEffect(() => {
     const img = new Image();
@@ -42,21 +61,23 @@ function App() {
       const [w, h] = getNewSize(img.width, img.height);
       setDivSize({ width: w, height: h });
       const [pw, ph, pt] = get1Part({ width: w, height: h });
+      setPartSize({ width: pw, height: ph });
 
-      const parts: PartSize[] = [...Array(pt)].map((_, m) => {
-        const t = getRow(m) - 1;
-        const l = m - t * 8;
+      const parts: PartSize[] = [...Array(pt)].map((_, i) => {
+        const [top, left] = getTopLeft(i, pw, ph);
         const partSize = {
           width: pw,
           height: ph,
-          top: 1 * (t * ph),
-          left: 1 * (l * pw),
+          bgPosX: -1 * left,
+          bgPosy: -1 * top,
         };
 
         return partSize;
       });
 
-      setParts(parts);
+      const shuffledPart = shuffle(parts);
+
+      setParts(shuffledPart);
     };
   }, []);
 
@@ -80,7 +101,9 @@ function App() {
           }}
         >
           {parts.map((m, i) => {
-            return <PartItem key={i} {...m} />;
+            const [top, left] = getTopLeft(i, partSize.width, partSize.height);
+
+            return <PartItem key={i} {...m} top={top} left={left} />;
           })}
         </div>
       </main>
