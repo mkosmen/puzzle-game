2a0eb0e473fe468229603fc2f6839d31160f93b0
Mehmet KOSMEN <mkosmen@gmail.com>
2025-12-13 22:29:22 +0300

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <></>;
+  return (
+    <div className="flex h-screen flex-col">
+      <header className="p-3"></header>
+      <main className="flex-1"></main>
+    </div>
+  );
 }
 
 export default App;
